52720b3c986009fc5b93842374d50fc390f82000
chinm0818 <32941983+chinm0818@users.noreply.github.com>
2018-02-13 22:05:53 -0500

    final submission. presentation and readme file updated

--- pristiq/pristiq/spiders/pristiq_spider.py
+++ pristiq/pristiq/spiders/pristiq_spider.py
@@ -51,42 +51,4 @@ class pristiq_spider(Spider):
 
 		
 
-		#dates = response.xpath('//div[@class = "date"]/text()').extract() # saves a list of dates 
-		#reviewers = response.xpath('//p[@class = "reviewerInfo"]/text()').extract() # saves a list of reviewrs
-		#conditions = conditions = response.xpath('//div[@class = "conditionInfo"]/text()').extract() # saves a list of conditions
-		#except for comment, which has it's own tag condition
-		#com_tag = '//p[@id = "comFull{}"]/text()' 
-		#comments = [response.xpath(com_tag.format(n)).extract() for n in range(1,6)] # gives a list of comments 
-		#ratings = response.xpath('//span[@class = "current-rating"]/text()').extract() # gives all ratings listed on the site
-		#effects = [ratings[n] for n in range(0, len(ratings),3)] # saves a list of ratings for effectiveness 
-		#eases = [ratings[n] for n in range(1, len(ratings),3)] # saves a list of ratings for ease of use
-		#satisfactions = [ratings[n] for n in range(2, len(ratings),3)] # saves a list of ratings for satisfaction
-
-		#item = CymbaltaItem()
-
-#setting loop to append to items  
-		#for i in range(5):
-
-			#item['date'] = dates
-			#item['reviewer'] = reviewers[i]
-			#item['condition'] = conditions[i]
-			#item['effective'] = effects[i]
-			#item['ease'] = eases[i]
-			#item['satisfaction'] = satisfactions[i]
-			#item['comment'] = comments[i]
-
-			#print(item)
-			#yield item
-
-
-
-
-
-	
-
-
-
-
-#url = 'https://www.webmd.com/drugs/drugreview-91491-Cymbalta+oral.aspx?drugid=91491&drugname=Cymbalta+oral&pageIndex=0&sortby=3&conditionFilter=-1'
-
 
